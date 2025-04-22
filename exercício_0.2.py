fb1540dbef9fd945f2da745719e0497d969cc6f4
Tazerfade <artur.aguirre7@gmail.com>
2025-04-22 08:57:24 -0300

    Entrega de exercício de revisão 2

---
+++ exercício_0.2.py
@@ -0,0 +1,13 @@
+num =  float(input("Digiite seu número :"))
+
+if num >0:
+    if num%2==0:
+         print("o número descrito é par e positivo.")
+    else:
+        print("O número descrito é impar e positivo.")
+else:
+    if num%2==1:
+        if num < 0:
+            print(" O número descrito é ímpar e negativo.")
+        else:
+            print("O número descrito é par e negativo ")
